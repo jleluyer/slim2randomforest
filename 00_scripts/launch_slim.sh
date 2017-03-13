b1a3edb3b07750efc7a9dfe2b12a6e492d899957
jeremyleluyer <jeremy.le.luyer@gmail.com>
2017-03-13 04:44:39 -0400

    first commit

--- 00_scripts/launch_slim.sh
+++ 00_scripts/launch_slim.sh
@@ -4,7 +4,7 @@
 awk '{print $1}' 01_info_file/strata.txt|grep -v 'INDIV' >01_info_file/individuals.list.txt
 
 #launch loop
-for i in 1
+for i in $1
 do
   toEval="cat 00_scripts/slim2randomforest.sh | sed 's/__IDX__/$i/g'"
     eval $toEval > TOTAL_"$i".sh
